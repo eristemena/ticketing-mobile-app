49009092960e07078d9ffdf8567ad170b98011b0
Eris Ristemena <e.ristemena@gmail.com>
2021-07-15 04:34:16 +0700

    Add lint

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-import {StyleSheet} from 'react-native';
 import MainStack from './src/navigations/MainStack';
 
 export default function App() {
